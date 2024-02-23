904075aa52afa6adfc302d070aefeddd2dbda593
Youpizo <systemluffy@gmail.com>
2024-02-23 13:31:49 +0100

    démarrage

---
+++ assets/JS/projets.js
@@ -0,0 +1,7 @@
+    const btnTOUS = document.querySelector(".filter_btn_0");
+    const btn_1 = document.querySelector(".filter_btn_1");
+    const btn_2 = document.querySelector(".filter_btn_2");
+    const btn_3 = document.querySelector(".filter_btn_3");
+    
+    
+
